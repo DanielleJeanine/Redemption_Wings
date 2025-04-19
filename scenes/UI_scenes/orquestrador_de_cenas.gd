d7ad86fe8d8fed312b07c56b4072475146cfa09b
Joseph da costa ribeiro <87831574+josephDcostaR@users.noreply.github.com>
2025-04-18 21:03:36 -0300

    Add/feature: Controles menu e Parallax cenario

--- scenes/UI_scenes/orquestrador_de_cenas.gd
+++ scenes/UI_scenes/orquestrador_de_cenas.gd
@@ -1,13 +1,45 @@
 extends Node
 
 @onready var menu_principal = $MenuInicial
+@onready var Controles = $Controles
+@onready var game = $Game
+@onready var player= $Game/Player
+@onready var parallax = $Game/ParallaxBackground
 @onready var play_btn = $MenuInicial/VBoxContainer/Play_btn
+@onready var controles_btn = $MenuInicial/VBoxContainer/Controles_btn
 
 func _ready():
 	play_btn.grab_focus()
+	game.visible = false
+	player.set_process(false)
+	player.set_physics_process(false)
+	player.set_process_input(false)
+	parallax.set_process(false)
 
 func _on_play_btn_pressed():
 	menu_principal.visible = false
+	game.visible = true
+	
+	player.set_process(true)
+	player.set_physics_process(true)
+	player.set_process_input(true)
+	parallax.set_process(true)
+	
+func _on_controles_btn_pressed():
+	menu_principal.visible = false
+	Controles.visible = true
+	
 	
 func _on_exit_btn_pressed():
 	get_tree().quit()
+
+
+func _on_voltar_menu_btn_pressed():
+	Controles.visible = false
+	menu_principal.visible = true
+	
+	player.set_process(false)
+	player.set_physics_process(false)
+	player.set_process_input(false)
+	parallax.set_process(false)
+	
